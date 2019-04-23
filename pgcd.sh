0a1362fe84a04cc1012f03805ceb4a070c4dc75a
Andre Tran <andreviet2009@hotmail.com>
2019-04-23 16:20:51 -0400

    Create pgcd.sh

---
+++ pgcd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#pgcd.sh script
+#Auteur : 
+# - Andre-Viet Tran 2001185
+# - Alexandre Keo 
+#Fait le 23 avril 2019
+#But : Calculer le pgcd de deux nombres positifs
+if [ $# -ne 2 ]; then
+	echo "Mauvais nombre d'argument"
+	exit
+fi
+x=$1
+y=$2
+if [ "$x" -lt 0 ] || [ "$y" -lt 0 ]; then
+	echo "Entrez deux entiers positifs"
+	exit
+fi
+while [ "$x" -ne "$y" ]; 
+do
+	if [ "$x" -gt "$y" ]; then
+		x=$x-$y
+	else
+		y=$y-$x
+	fi
+done
+echo "pgcd de $1 et de $2 est $x"
